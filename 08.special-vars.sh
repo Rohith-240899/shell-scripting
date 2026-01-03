6f58691de8797981736fc2e63545574577e0a3bf
hemaja <hemaja@gmail.com>
2026-01-03 17:59:31 +0530

    varirables

--- 08.special-vars.sh
+++ 08.special-vars.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "All variables passed to the script: $@"
+echo "All variables passed to the script:$@"
 echo "All variables passed to the script:$*"
+echo "Script Name is:$0"
+echo "Current Directory:$PWD"
+echo "Current User:$USER"
+echo "Current Home directory of $USER is:$HOME"
+echo "PID of the script is:$$"

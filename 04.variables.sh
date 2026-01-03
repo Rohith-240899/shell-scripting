710dd37b288cc6039e19305969b025c2be60f0fa
hemaja <hemaja@gmail.com>
2026-01-03 14:29:08 +0530

    passing arguments

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1:: Hi Anna. How are you?"
+echo " $PERSON2:: Hi ra. Iam fine how are you?"
+echo "$PERSON1 :: Fine anna. Vadina ela undi?"
+echo " $PERSON2:: Bagundi ra. Chadvukuntunnava leda asalu?"
+echo "$PERSON1:: Yes Anna!! Reading and Practcing everyday "

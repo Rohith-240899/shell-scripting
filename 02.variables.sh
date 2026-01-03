6a9b9277abbba5339382746859ff3a1c45027f21
hemaja <hemaja@gmail.com>
2026-01-03 14:06:46 +0530

    new convo chnages using variables

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,2 +1,8 @@
 Name1 = Rohith
-Name2 = Hemaja
+Name2 = Ram
+
+echo " $Name1:: Hi Anna. How are you?"
+echo " $Name2:: Hi ra. Iam fine how are you?"
+echo "$Name1 :: Fine anna. Vadina ela undi?"
+echo " $Name2:: Bagundi ra. Chadvukuntunnava leda asalu?"
+echo "$Name1:: Yes Anna!! Reading and Practcing everyday "

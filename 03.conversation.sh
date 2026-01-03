b0f471409735b3e0a513d866f2b786cbe0d3bd02
hemaja <hemaja@gmail.com>
2026-01-03 13:56:10 +0530

    covo

---
+++ 03.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " Rohith:: Hi Anna. How are you?"
+echo " Anna:: Hi ra. Iam fine how are you?"
+echo "Rohith :: Fine anna. Vadina ela undi?"
+echo " Anna:: Bagundi ra. Chadvukuntunnava leda asalu?"
+echo "Rohith:: Yes Anna!! Reading and Practcing everyday "

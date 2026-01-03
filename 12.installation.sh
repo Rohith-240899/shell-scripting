0e214a78d2a0fd46309810061204f21280a2daec
hemaja <hemaja@gmail.com>
2026-01-03 20:12:35 +0530

    varirables

--- 12.installation.sh
+++ 12.installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo " ERROR:: User dooesnt have root access"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "Installing mysql is FAILURE"
+    exit 1
 else
     echo " Installling mysql is SUCCESS"
 fi

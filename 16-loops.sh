5465c356eb482f44687ce27e091da34cd683f72d
hemaja <hemaja@gmail.com>
2026-01-04 13:12:14 +0530

    loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -2,7 +2,7 @@
  R="\e[31m"
  G="\e[32m"
  Y="\e[33m"
- N="\e[38m"
+ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -36,6 +36,6 @@ do
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package"
     else
-        echo " $package is already exit...$Y SKIPPING $N" | tee -a $LOGS_FILE
+        echo -e " $package is already exist...$Y SKIPPING $N" | tee -a $LOGS_FILE
     fi
 done
